87b3adc50cc5cd2fe35effd7e6aec6f97fbea6b6
DSdsfre <rraxo96@gmail.com>
2025-10-12 21:56:18 +0300

    Fix build configuration - remove unused reanimated plugin, simplify configs

--- metro.config.js
+++ metro.config.js
@@ -2,26 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Enable web support
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Configure web-specific settings
-config.resolver.alias = {
-  ...config.resolver.alias,
-  'react-native': 'react-native-web',
-};
-
-// Optimize for web builds
-if (process.env.EXPO_PLATFORM === 'web') {
-  config.transformer.minifierConfig = {
-    keep_fnames: true,
-    mangle: {
-      keep_fnames: true,
-    },
-  };
-  
-  // Enable tree shaking for web
-  config.resolver.sourceExts = [...config.resolver.sourceExts, 'web.js', 'web.ts', 'web.tsx'];
-}
-
 module.exports = config;
